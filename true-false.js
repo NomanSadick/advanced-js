43ebed074386b968fa2daad430d3835df82b8cf1
Noman Sadick <nomansadickjs@gmail.com>
2020-03-06 09:34:57 +0600

    add null

---
+++ true-false.js
@@ -0,0 +1,17 @@
+//Falsy:
+// 0
+// ""
+// undefined
+// null
+// NaN
+// false
+
+//TRULY
+// '0',' ',[]
+const name = " ";
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
